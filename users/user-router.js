e986e037518fff11248e71a189fad25d6d26abbd
danRolon <daniel.rolon@yahoo.com>
2019-10-16 17:21:14 -0500

    setup restriction via token

--- users/user-router.js
+++ users/user-router.js
@@ -1,13 +1,12 @@
 const router = require('express').Router();
 
 const Users = require('./user-model.js');
-//const restricted = require('../auth/restricted.js');
+const restricted = require('../auth/restricted.js');
 
-router.get('/', (req, res) => {
+router.get('/', restricted, (req, res) => {
   Users.find()
     .then(users => {
-     // res.json({loggedInUser: req.username, users:users});
-      res.json(users);
+     res.json({loggedInUser: req.username, users:users});
     })
     .catch(err => res.send(err));
 });
